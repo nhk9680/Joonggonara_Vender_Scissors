185922bf5cb74b52cf086e3b6f714bd9e8c354ba
nhk9680 <nhk9680@gmail.com>
2020-10-18 19:40:55 +0900

    update site url

--- dc_chart_named.py
+++ dc_chart_named.py
@@ -7,79 +7,108 @@ from selenium import webdriver
 # from bs4 import BeautifulSoup
 import telegram
 
+url = 'https://m.dcinside.com/board/electronicmoney'
+
 # args (for auto restart)
 executable = sys.executable
 args = sys.argv[:]
 args.insert(0, sys.executable)
 
+with open("telegram.txt", "r") as f:
+    lines = f.readlines()
+my_token = lines[0][:-1]     # sender, -1: remove \n
+chat_id = lines[1]      # receiver
+
+
 # telegram
-my_token = '1046523902:AAHdt0hYT4Xc5ndpAUaLZ4RQJqkwyt6p7bU'
-bot         = telegram.Bot(my_token)
-chat_id     = bot.getUpdates()[-1].message.chat.id
-# text_tmp    = bot.getUpdates()[-1].message.text
+try:
+    bot = telegram.Bot(my_token)
+    # chat_id     = bot.getUpdates()[-1].message.chat.id
+except Exception as e:
+    print(chat_id)
+    sys.exit()
 
 # selenium.webdriver
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument('window-size=1920x1080')
-options.add_argument('disable-gpu')
+# options.add_argument('headless')
+# options.add_argument('window-size=1920x1080')
+# options.add_argument('disable-gpu')
 options.add_argument('log-level=1')
+
+# mobile emulation
+mobile_emulation = {'deviceName': "iPhone X"}
+options.add_experimental_option("mobileEmulation", mobile_emulation)
 
 driver = webdriver.Chrome(executable_path='D:/chromedriver.exe', options=options)
 driver.implicitly_wait(1)
 
-keywords    = ['korea', '210.126', 'koreasatoshi', 'FlightF']
+keywords    = ['FlightF', '워뇨띠']
 sign        = ['↑', '↗', '→', '↘', '↓', '↙', '←', '↖']
 
 # get article index
-driver.get('https://gall.dcinside.com/mgallery/board/lists?id=chartanalysis')
-num_old = driver.find_element_by_xpath(
-        '//*[@id="container"]/section[1]/article[2]/div[2]/table/tbody/tr[7]/td[1]').text
+driver.get(url)
+
+def boardXpath(n):
+    return f'/html/body/div/div/div/section[3]/ul[2]/li[{n}]/div/a[1]'
+
+def get_board_info(n):
+    board = {
+        'url': driver.find_element_by_xpath(boardXpath(n)).get_attribute('href'),
+        'num': driver.find_element_by_xpath(boardXpath(n)).get_attribute('href').split('/')[-1],
+        'name': driver.find_element_by_xpath(boardXpath(1)+'/ul/li[2]').text,
+        'title': driver.find_element_by_xpath(boardXpath(1)+'/span/span[2]').text
+    }
+    return board
+
+
+# num_old = get_board_info(1)['num']
+
+boardnum_old = set()
+board_list = []
+for i in range(5):
+    board_list.append(get_board_info(i+1))
+    boardnum_old.add(board_list[i]['num'])
+
 
 # bot.sendMessage(chat_id, "test")
 # sys.exit()
 
 try:
-
     t = 0
     while True:
         # activation check
-        time.sleep(1)
-
+        time.sleep(10)
         now = time.gmtime(time.time())
         print('{:d}시 {:d}분 {:d}초 \t {:s}'.
               format(now.tm_hour + 9, now.tm_min, now.tm_sec, sign[t % 8]), end='\r')
-
         if t == 8:
             t = 0
         t += 1
 
-        # gather info
-
         driver.refresh()
 
+        boardnum = set()
+        board_list = []
+        for i in range(5):
+            board_list.append(get_board_info(i + 1))
+            boardnum.add(board_list[i]['num'])
 
-        num = driver.find_element_by_xpath(
-            '//*[@id="container"]/section[1]/article[2]/div[2]/table/tbody/tr[7]/td[1]').text
-        if num == num_old:
+        if boardnum == boardnum_old:
             continue
-        num_old = num
-
-        name = driver.find_element_by_xpath(
-            '//*[@id="container"]/section[1]/article[2]/div[2]/table/tbody/tr[7]/td[4]').text
-        title = driver.find_element_by_xpath(
-            '//*[@id="container"]/section[1]/article[2]/div[2]/table/tbody/tr[7]/td[3]/a').text
-
+        boardnum.remove(min(boardnum))
+        boardnum_old = boardnum
 
         # check
-        for word in keywords:
-            if word in name:
-                for i in range(0, 5):
-                    Beep(4186, 100)
+        for board in board_list:
+            name = board['name']
+            for word in keywords:
+                if word in name:
+                    for i in range(0, 5):
+                        Beep(4186, 100)
 
-                link = 'https://gall.dcinside.com/mgallery/board/view/?id=chartanalysis&no=' + num + '&page=1'
-                print("\n"+title+"\n"+name+"\n"+link+"\n")
-                bot.sendMessage(chat_id=chat_id, text=title+"\n"+name+"\n"+link)
+                    link = url + board['num']
+                    print("\n"+board['title']+"\n"+name+"\n"+link+"\n")
+                    bot.sendMessage(chat_id=chat_id, text=board['title']+"\n"+name+"\n"+link)
 
 
 except Exception as e:
